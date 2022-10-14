7b98767f176725ada794fcf2b4313ab9689786f3
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2022-10-14 23:15:04 +0200

    create single cart widget

---
+++ lib/core/custom_widgets/single_cart_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:res_capital_task_abdelhamid_ahmed/features/cart_screen/domain/entities/cart_entity.dart';
+
+class SingleCartWidget extends StatelessWidget {
+  final Cart? cart;
+
+  const SingleCartWidget({super.key, this.cart});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
